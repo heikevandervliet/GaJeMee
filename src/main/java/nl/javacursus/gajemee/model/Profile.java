46f9f74d7c651c3d496800574ee21cb4b5a0cd58
Esther <ecmvanderhoorn@gmail.com>
2017-12-12 15:16:19 +0100

    profiel aangepast

--- src/main/java/nl/javacursus/gajemee/model/Profile.java
+++ src/main/java/nl/javacursus/gajemee/model/Profile.java
@@ -1,5 +1,22 @@
 package nl.javacursus.gajemee.model;
 
+import java.time.LocalDate;
+
 public class Profile {
 
-}
+	private String userName;
+	private String email;
+	private String passWord;
+	private String userLocation;
+	private LocalDate birthday;
+	private LocalDate creationProfile;
+	private boolean loggedOn;
+
+	public Profile() {
+		
+	}
+	
+			
+	
+
+	}
