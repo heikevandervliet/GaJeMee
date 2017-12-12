7280f0f5bfa01d1486b3f9a8c0bbb52dd9066e15
Esther <ecmvanderhoorn@gmail.com>
2017-12-12 16:01:59 +0100

    iets toegevoegd aan profiel

--- src/main/java/nl/javacursus/gajemee/model/Profile.java
+++ src/main/java/nl/javacursus/gajemee/model/Profile.java
@@ -13,10 +13,63 @@ public class Profile {
 	private boolean loggedOn;
 
 	public Profile() {
-		
-	}
-	
-			
-	
 
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getUserLocation() {
+		return userLocation;
+	}
+
+	public void setUserLocation(String userLocation) {
+		this.userLocation = userLocation;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public LocalDate getCreationProfile() {
+		return creationProfile;
+	}
+
+	public void setCreationProfile(LocalDate creationProfile) {
+		this.creationProfile = creationProfile;
+	}
+
+	public boolean isLoggedOn() {
+		return loggedOn;
+	}
+
+	public void setLoggedOn(boolean loggedOn) {
+		this.loggedOn = loggedOn;
+	}
+
+}
